6275d9217b13da79f9663a1994bf2137a26da733
Fred Geffray <fred.geffray@gmail.com>
2020-09-21 17:08:14 +0200

    init

---
+++ app/src/main/java/com/quantical/topquizz/model/QuestionBank.java
@@ -0,0 +1,29 @@
+package com.quantical.topquizz.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionBank {
+
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList) {
+        mQuestionList = questionList;
+
+        // Shuffle the question list
+        Collections.shuffle(mQuestionList);
+
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getQuestion() {
+        // Ensure we loop over the questions
+        if (mNextQuestionIndex == mQuestionList.size()) {
+            mNextQuestionIndex = 0;
+        }
+
+        // Please note the post-incrementation
+        return mQuestionList.get(mNextQuestionIndex++);
+    }
+}
---
+++ app/src/main/java/com/quantical/topquizz/model/User.java
@@ -0,0 +1,21 @@
+package com.quantical.topquizz.model;
+
+public class User {
+
+    private String mFirstname;
+
+    public String getFirstname() {
+        return mFirstname;
+    }
+
+    public void setFirstname(String firstname) {
+        mFirstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstname='" + mFirstname + '\'' +
+                "}";
+    }
+}
