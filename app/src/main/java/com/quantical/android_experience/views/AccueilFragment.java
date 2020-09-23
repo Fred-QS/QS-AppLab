4f32d2e0f1d8eaf720a8352fb0e8a52fcf5fa09e
Fred Geffray <fred.geffray@gmail.com>
2020-09-23 15:30:59 +0200

    update

--- app/src/main/java/com/quantical/android_experience/views/AccueilFragment.java
+++ app/src/main/java/com/quantical/android_experience/views/AccueilFragment.java
@@ -14,27 +14,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.quantical.android_experience.R;
-import com.quantical.android_experience.models.Accueil;
 
 public class AccueilFragment extends Fragment {
 
-    private Accueil mAccueil;
     Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mAccueil =
-                ViewModelProviders.of(this).get(Accueil.class);
+
         View root = inflater.inflate(R.layout.fragment_accueil, container, false);
         context = root.getContext();
         final TextView textView = root.findViewById(R.id.text_accueil);
-
-        mAccueil.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         return root;
     }
